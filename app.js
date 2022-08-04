7fa9509ca586c655b7a26703835dd2f9181559c0
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-08-04 21:54:19 +0530

    Initial release

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+app.use(express.json());
 
 app.get("/", function (req, res) {
-    res.render("body");
+  res.render("body");
 });
 
-app.post("/", function (req, res) {
-    res.send("/");
-});
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.post("/", function (req, res){
   
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
--- public/JS/index.js
+++ public/JS/index.js
@@ -1,3 +1,4 @@
+
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
@@ -18,55 +19,128 @@ inputs.forEach((input) => {
 });
 
 const blobity = new Blobity({
-  color: "#eeda00a6",
-  zIndex: 1000,
+  color: "#eeda00a8",
+  zIndex: 10000,
   opacity: 0.5,
   magnetic: true,
   dotColor: "#FF2E63",
-  dotSize: 12.5,
+  dotSize: 15,
   font: "Montserrat",
   fontWeight: 400,
-  tooltipPadding: 10,
+  tooltipPadding: 4,
   radius: 10,
   focusableElementsOffsetX: 4,
   focusableElementsOffsetY: 4,
 });
 
 async function reveal() {
-  var reveals = $('.reveal');
+  var reveals = $(".reveal");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
 
     if (elementTop < windowHeight) {
       reveals[i].classList.add("active");
-    } 
-    else {
+    } else {
       reveals[i].classList.remove("active");
     }
   }
 }
 
-window.addEventListener('scroll', reveal);
+window.addEventListener("scroll", reveal);
 
-
-$(window).load(()=>{
+$(window).load(() => {
   $(window).scroll(() => {
-    var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
-    var totalScroll = (wintop/(docheight-winheight))*100;
-    $(".progressBar").css("width",totalScroll+"%");
+    var wintop = $(window).scrollTop(),
+      docheight = $("body").height(),
+      winheight = $(window).height();
+    var totalScroll = (wintop / (docheight - winheight)) * 100;
+    $(".progressBar").css("width", totalScroll + "%");
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   var initScrollTop = $(window).scrollTop();
-  parallax = $('.section-1');
-  $(parallax).css({'background-position-y' : (initScrollTop/75)+'%'});
-  $(window).scroll(function() {
+  parallax = $(".section-1");
+  $(parallax).css({ "background-position-y": initScrollTop / 75 + "%" });
+  $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
-    $(parallax).css({'background-position-y' : (scrollTop/75)+'%'});
-    
+    $(parallax).css({ "background-position-y": scrollTop / 75 + "%" });
   });
-  
 });
 
+const contactForm = $(".contact-form form");
+const name = $("#Name");
+const email = $("#Email");
+const message = $("#Message");
+
+(function(){
+  emailjs.init("VK4TXnDVYsZsA0iSQ");
+})();
+
+function sendMail(){
+  var params =  {
+    from_name: name.val(),
+    email_id: email.val(),
+    message: message.val(),
+  };
+  emailjs.send('service_zkfvor7', 'template_9bjsywc', params)
+    .then(function(response) {
+       success();
+       contactForm.trigger('reset');
+    }, function(error) {
+       failure();
+    });
+  }
+    
+
+
+const button = $(".toastBtn"),
+      toast = $(".toast")
+      closeIcon = $(".close"),
+      progress = $(".progress");
+
+      let timer1, timer2;
+
+      function success() {
+        toast.addClass("btnOn");
+        progress.addClass("btnOn");
+        timer1 = setTimeout(() => {
+            toast.removeClass("btnOn");
+        }, 5000); //1s = 1000 milliseconds
+        timer2 = setTimeout(() => {
+          progress.removeClass("btnOn");
+        }, 5300);
+      closeIcon.click(() => {
+        toast.removeClass("btnOn");
+        setTimeout(() => {
+          progress.removeClass("btnOn");
+        }, 300);
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+      });
+    }
+
+    
+
+    function failure() {
+      $('.text-1').html("Error");
+    $('.text-2').html('Uh-oh - Something went wrong :(');
+    $('.check').css({'color':'white', 'background-color':'red'});
+      toast.addClass("btnOn");
+      progress.addClass("btnOn");
+      timer1 = setTimeout(() => {
+          toast.removeClass("btnOn");
+      }, 5000); //1s = 1000 milliseconds
+      timer2 = setTimeout(() => {
+        progress.removeClass("btnOn");
+      }, 5300);
+    closeIcon.click(() => {
+      toast.removeClass("btnOn");
+      setTimeout(() => {
+        progress.removeClass("btnOn");
+      }, 300);
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    });
+  }
