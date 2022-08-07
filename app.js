c9d0afe2e3f62d852d82f588ce24eaee73e3bde9
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-08-07 14:05:15 +0530

    Added compression

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const compression = require('compression');
 const port = process.env.PORT || 3000;
 
 /* Middlewares */
 
+app.use(compression());
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
