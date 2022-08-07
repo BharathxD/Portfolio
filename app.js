0a357497cd21b69f56692ed170da568a1eea6c57
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-08-07 13:23:12 +0530

    Fixed some bugs and refactored the code

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
+/* Middlewares */
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.render("body");
+/* Home Route */
+
+app.get('/', function (req, res) {
+  res.render('body');
 });
 
-app.post("/", function (req, res){
-  
+/* Server running on port 3000 */
+
+let http = require('http');
+let server = express().use('/', app);
+http.createServer(server).listen(port, () => {
+  console.log('Listening on ' + port);
 });
 
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
